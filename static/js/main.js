56b8d455b919b5f460e48880b58c8d9d01cf106f
Aniss <aniss@aniss-Vostro-1015.(none)>
2011-12-04 14:19:08 +0100

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+require(["jquery", "http://cdnjs.cloudflare.com/ajax/libs/less.js/1.1.3/less-1.1.3.min.js"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+	
+    });
+});
